16ba344099c889556b8d4a4c37c1b0c600928e74
GHOST-mHBr <ghostmhbr@gmail.com>
2023-09-30 09:58:52 +0330

    some cleaning

---
+++ README.md
@@ -0,0 +1,51 @@
+# SQL injection lab
+
+## Requirements
+
+- MySQL Server running on localhost
+- MySQL Employees sample database
+- PHP (tested with PHP 8.1)
+
+## Installation
+
+### Step1: PHP, web server and MySQL installation
+
+<details open><summary><h3>Linux</h3></summary>
+
+on a linux system you can install apache as web server Mysql-Server, PHP8.1 and PHP8.1-mysql packages.
+
+in order to install PHP8.1, MySQL-server,mysql-server and apache you can run the following command:
+
+```shell
+sudo apt install php8.1,mysql-server,php8.1-mysql,apache2
+```
+
+</details>
+
+<details><summary><h3>Windows</h3></summary>
+
+on windows we have [xampp](https://www.apachefriends.org/), which is able to install and run apache web server and php. i don't know if there is a better or alternative solution, but xampp should work properly.
+
+you can download xampp from [here](https://www.apachefriends.org/download.html)
+
+in order to install MySQL server on windows you can use the free version of mysql.
+
+[this](https://dev.mysql.com/downloads/installer/) is the download page of mysql installer.
+
+</details>
+
+### Step2: Installing the Employee sample database
+
+in order to install the Employee sample database, refer to its installation [page](https://dev.mysql.com/doc/employee/en/employees-installation.html) on MySQL website.
+
+:warning: ***In order to connect php-mysql and mysql server you should insert your database credentials inside php files(all levels). just scroll down inside them and read the comments.***
+
+### Step3: Clone and copying
+
+Now you should clone the repository, then just copy the entire "SQLi Lab" folder and paste it in one of the following paths:
+
+for linux: `/var/www/html/`
+
+and for Windows and xampp: `C:/xampp/htdocs/`
+
+Now you can reach the lab by opening a browser and entering the following address :`127.0.0.1/SQLI%20Lab/level1.php`
---
+++ level1.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>SQLi Lab-1</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="/SQLi Lab/styles/style1.css">
+    <link rel="stylesheet" href="/SQLi Lab/styles/style_light.css">
+    <link rel="icon" type="image/x-icon" href="/SQLi Lab/images/favicon.ico">
+</head>
+
+<body class="w3-dark">
+    <div class="w3-margin" style="max-width:100%">
+        <header class="w3-center head">
+            <ul>
+                <li style="--clr:#00ade1">
+                    <a data-text="&nbsp;Welcome">&nbsp;Welcome&nbsp;</a>
+                </li>
+                <li style="--clr:#00ade1">
+                    level 1
+                </li>
+            </ul>
+        </header>
+        <?php
+        // ------ DataBase Credentials ------
+        // enter yours
+        $db_ip = "127.0.0.1";
+        $username = "root";
+        $password = "10101010";
+        $dbname = "employees";
+        $isDbOk = true;
+        try {
+            $db_conn = mysqli_connect($db_ip, $username, $password, $dbname);
+        } catch (mysqli_sql_exception) {
+            echo "<h2>MySQL-Server connection error</h2>";
+            $isDbOk = false;
+        }
+        if ($isDbOk) {
+            if (isset($_GET['id'])) {
+                $id = $_GET['id'];
+                $query = "SELECT first_name,last_name from employees where emp_no=" . $id . ";";
+
+                $result = $db_conn->query($query);
+                if ($result->num_rows > 0) {
+                    if ($result->num_rows > 1) {
+                        echo "<div class=\"w3-center w3-padding w3-margin-bottom w3-margin-top w3-deep-purple\">";
+                        echo "<h3>Good job!<br>You just hacked the database</h3></div>";
+                    }
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<div style=\"margin-top: 20px\">";
+                        echo "<h2>";
+                        echo "First name: " . $row["first_name"] . "<br>";
+                        echo "Last name: " . $row["last_name"] . "<br><br>";
+                        echo "</h2>";
+                        echo "</div>";
+                    }
+                } else {
+                    echo "<h2>Bad id :(</h2>";
+                }
+            } else {
+                echo "<h2>No id specified. What's up dude?</h2>";
+            }
+        }
+        ?>
+    </div>
+    <footer class="w3-center w3-black w3-padding-16">
+        <a href="https://www.github.com/GHOST-mHBr/SQLi-Lab">
+            <img src="/SQLi Lab/images/github.svg" width="30px" height="30px">
+        </a>
+    </footer>
+</body>
+
+</html>
